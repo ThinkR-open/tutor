2316e972bc2125fe387cb9241425918783b5890b
cervan <cervan@thinkr.fr>
2022-02-17 10:01:04 +0000

    Changement de comment lancer les learnr

--- R/all_functions_en.R
+++ R/all_functions_en.R
@@ -1,64 +1,96 @@
+#' launch bar_charts_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-vis_basics_en <- function(file=tutor:::tous_les_programmes(lang='en')[1],...){tutor::launch_learn(file=file,...)}
+bar_charts_en <- function(file=tutor:::tous_les_programmes(lang='en')[1],...){tutor::launch_learn(file=file,...)}
 
+#' launch exploratory_data_analysis_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-programming_basics_en <- function(file=tutor:::tous_les_programmes(lang='en')[2],...){tutor::launch_learn(file=file,...)}
+exploratory_data_analysis_en <- function(file=tutor:::tous_les_programmes(lang='en')[2],...){tutor::launch_learn(file=file,...)}
 
+#' launch reshape_data_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 reshape_data_en <- function(file=tutor:::tous_les_programmes(lang='en')[3],...){tutor::launch_learn(file=file,...)}
 
+#' launch tibbles_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-separate_columns_en <- function(file=tutor:::tous_les_programmes(lang='en')[4],...){tutor::launch_learn(file=file,...)}
+tibbles_en <- function(file=tutor:::tous_les_programmes(lang='en')[4],...){tutor::launch_learn(file=file,...)}
 
+#' launch isolating_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-join_datasets_en <- function(file=tutor:::tous_les_programmes(lang='en')[5],...){tutor::launch_learn(file=file,...)}
+isolating_en <- function(file=tutor:::tous_les_programmes(lang='en')[5],...){tutor::launch_learn(file=file,...)}
 
+#' launch separate_columns_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-tibbles_en <- function(file=tutor:::tous_les_programmes(lang='en')[6],...){tutor::launch_learn(file=file,...)}
+separate_columns_en <- function(file=tutor:::tous_les_programmes(lang='en')[6],...){tutor::launch_learn(file=file,...)}
 
+#' launch vis_basics_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-isolating_en <- function(file=tutor:::tous_les_programmes(lang='en')[7],...){tutor::launch_learn(file=file,...)}
+vis_basics_en <- function(file=tutor:::tous_les_programmes(lang='en')[7],...){tutor::launch_learn(file=file,...)}
 
+#' launch deriving_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 deriving_en <- function(file=tutor:::tous_les_programmes(lang='en')[8],...){tutor::launch_learn(file=file,...)}
 
+#' launch histograms_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-bar_charts_en <- function(file=tutor:::tous_les_programmes(lang='en')[9],...){tutor::launch_learn(file=file,...)}
+histograms_en <- function(file=tutor:::tous_les_programmes(lang='en')[9],...){tutor::launch_learn(file=file,...)}
 
+#' launch join_datasets_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-exploratory_data_analysis_en <- function(file=tutor:::tous_les_programmes(lang='en')[10],...){tutor::launch_learn(file=file,...)}
+join_datasets_en <- function(file=tutor:::tous_les_programmes(lang='en')[10],...){tutor::launch_learn(file=file,...)}
 
+#' launch boxplots_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-histograms_en <- function(file=tutor:::tous_les_programmes(lang='en')[11],...){tutor::launch_learn(file=file,...)}
+boxplots_en <- function(file=tutor:::tous_les_programmes(lang='en')[11],...){tutor::launch_learn(file=file,...)}
 
+#' launch programming_basics_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
-boxplots_en <- function(file=tutor:::tous_les_programmes(lang='en')[12],...){tutor::launch_learn(file=file,...)}
+programming_basics_en <- function(file=tutor:::tous_les_programmes(lang='en')[12],...){tutor::launch_learn(file=file,...)}
 
+#' launch scatterplots_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 scatterplots_en <- function(file=tutor:::tous_les_programmes(lang='en')[13],...){tutor::launch_learn(file=file,...)}
 
+#' launch line_graphs_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 line_graphs_en <- function(file=tutor:::tous_les_programmes(lang='en')[14],...){tutor::launch_learn(file=file,...)}
 
+#' launch overplotting_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 overplotting_en <- function(file=tutor:::tous_les_programmes(lang='en')[15],...){tutor::launch_learn(file=file,...)}
 
+#' launch customize_en
+#' @param file name of the learnr to launch
+#' @param ... other params
 #' @export
-#' @noRd
 customize_en <- function(file=tutor:::tous_les_programmes(lang='en')[16],...){tutor::launch_learn(file=file,...)}
 
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+globalVariables(c(
+  ".rs.api.getJobState", ".rs.api.showDialog", ".rs.api.stopJob", ".rs.restartR",
+    ".rs.tutorial.registryGet", ".rs.tutorial.runTutorial"
+)
+)
