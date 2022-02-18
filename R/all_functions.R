e7c3a083dda7a436b61450d02c10dd840f03202c
vincent guyader <vincent@thinkr.fr>
2022-02-18 06:57:20 +0100

    Bump version (#20)
    * optim

--- R/all_functions.R
+++ R/all_functions.R
@@ -1,90 +1,75 @@
 #' Lance bar_charts_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-bar_charts_fr <- function(file=tutor:::tous_les_programmes()[1],...){tutor::launch_learn(file=file,...)}
+bar_charts_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[1],...)}
 
 #' Lance reshape_data_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-reshape_data_fr <- function(file=tutor:::tous_les_programmes()[2],...){tutor::launch_learn(file=file,...)}
+reshape_data_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[2],...)}
 
 #' Lance tibbles_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-tibbles_fr <- function(file=tutor:::tous_les_programmes()[3],...){tutor::launch_learn(file=file,...)}
+tibbles_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[3],...)}
 
 #' Lance exploratory_data_analysis_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-exploratory_data_analysis_fr <- function(file=tutor:::tous_les_programmes()[4],...){tutor::launch_learn(file=file,...)}
+exploratory_data_analysis_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[4],...)}
 
 #' Lance isolating_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-isolating_fr <- function(file=tutor:::tous_les_programmes()[5],...){tutor::launch_learn(file=file,...)}
+isolating_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[5],...)}
 
 #' Lance separate_columns_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-separate_columns_fr <- function(file=tutor:::tous_les_programmes()[6],...){tutor::launch_learn(file=file,...)}
+separate_columns_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[6],...)}
 
 #' Lance deriving_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-deriving_fr <- function(file=tutor:::tous_les_programmes()[7],...){tutor::launch_learn(file=file,...)}
+deriving_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[7],...)}
 
 #' Lance histograms_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-histograms_fr <- function(file=tutor:::tous_les_programmes()[8],...){tutor::launch_learn(file=file,...)}
+histograms_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[8],...)}
 
 #' Lance join_datasets_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-join_datasets_fr <- function(file=tutor:::tous_les_programmes()[9],...){tutor::launch_learn(file=file,...)}
+join_datasets_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[9],...)}
 
 #' Lance boxplots_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-boxplots_fr <- function(file=tutor:::tous_les_programmes()[10],...){tutor::launch_learn(file=file,...)}
+boxplots_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[10],...)}
 
 #' Lance programming_basics_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-programming_basics_fr <- function(file=tutor:::tous_les_programmes()[11],...){tutor::launch_learn(file=file,...)}
+programming_basics_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[11],...)}
 
 #' Lance scatterplots_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-scatterplots_fr <- function(file=tutor:::tous_les_programmes()[12],...){tutor::launch_learn(file=file,...)}
+scatterplots_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[12],...)}
 
 #' Lance line_graphs_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-line_graphs_fr <- function(file=tutor:::tous_les_programmes()[13],...){tutor::launch_learn(file=file,...)}
+line_graphs_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[13],...)}
 
 #' Lance overplotting_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-overplotting_fr <- function(file=tutor:::tous_les_programmes()[14],...){tutor::launch_learn(file=file,...)}
+overplotting_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[14],...)}
 
 #' Lance customize_fr
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-customize_fr <- function(file=tutor:::tous_les_programmes()[15],...){tutor::launch_learn(file=file,...)}
+customize_fr <- function(...){tutor::launch_learn(file=tous_les_programmes()[15],...)}
 
--- data-raw/reflexion.R
+++ data-raw/reflexion.R
@@ -5,20 +5,18 @@ tutor:::nice_name(tous,lang="en")
 cat(
 glue::glue("
 #' Lance *tutor:::nice_name(tutor:::tous_les_programmes())*
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-*tutor:::nice_name(tutor:::tous_les_programmes())* <- function(file=tutor:::tous_les_programmes()[*seq_along(tutor:::tous_les_programmes())*],...){tutor::launch_learn(file=file,...)}
+*tutor:::nice_name(tutor:::tous_les_programmes())* <- function(...){tutor::launch_learn(file=tous_les_programmes()[*seq_along(tutor:::tous_les_programmes())*],...)}
 
            ",.open = "*",.close = "*"),file="R/all_functions.R",sep="\n")
 
 cat(
 glue::glue("
 #' launch *tutor:::nice_name(tutor:::tous_les_programmes(lang='en'),lang='en')*
-#' @param file name of the learnr to launch
 #' @param ... other params
 #' @export
-*tutor:::nice_name(tutor:::tous_les_programmes(lang='en'),lang='en')* <- function(file=tutor:::tous_les_programmes(lang='en')[*seq_along(tutor:::tous_les_programmes(lang='en'))*],...){tutor::launch_learn(file=file,...)}
+*tutor:::nice_name(tutor:::tous_les_programmes(lang='en'),lang='en')* <- function(...){tutor::launch_learn(file=tous_les_programmes(lang='en')[*seq_along(tutor:::tous_les_programmes(lang='en'))*],...)}
 
            ",.open = "*",.close = "*"),file="R/all_functions_en.R",sep="\n")
 
